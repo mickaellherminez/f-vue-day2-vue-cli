7ce9e50c50e0e7a84e3f90b929cc2868753cf4bc
Mickael Lherminez <mickael.Lherminez@gmail.com>
2021-12-07 08:22:33 +0100

    Creating routes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <HelloWorld msg="Bonjour !"/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
