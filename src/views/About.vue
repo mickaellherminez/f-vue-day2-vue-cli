a3aec5e5f474a66c86596eed6ee18dc89dc89b58
Mickael Lherminez <mickael.Lherminez@gmail.com>
2021-12-08 08:41:13 +0100

    Add Axios

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,27 +1,36 @@
 <template>
   <div class="about">
-    <HelloWorld msg="Bonjour !"/>
+    <HelloWorld msg="Bonjour !" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: 'About',
+  name: "About",
   components: {
-    HelloWorld
+    HelloWorld,
   },
   data() {
-    return {
-    }
+    return {};
   },
-  methods: {
+  methods: {},
+  created() {
+    axios
+      .get(`http://jsonplaceholder.typicode.com/posts`)
+      .then((response) => {
+        // JSON responses are automatically parsed.
+        console.log('response.data)', response.data);
+      })
+      .catch((e) => {
+        console.log('e: ', e);
+      });
   },
-  mounted () {
-  },
-  watch: {
-  },
-}
+
+  mounted() {},
+  watch: {},
+};
 </script>
